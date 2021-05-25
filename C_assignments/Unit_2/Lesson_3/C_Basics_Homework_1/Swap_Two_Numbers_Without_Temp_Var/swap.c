4312b17c1219bb4fef1854c3a8a316d21f64da91
abdelrahman-yousry <abdelrahman.yousry000@gmail.com>
2021-05-25 05:20:42 +0200

    renaming folder

---
+++ C_assignments/Unit_2/Lesson_3/C_Basics_Homework_1/Swap_Two_Numbers_Without_Temp_Var/swap.c
@@ -0,0 +1,29 @@
+/*
+ * swap.c
+ *
+ *  Created on: Nov 2, 2020
+ *      Author: 20111
+ */
+
+
+#include <stdio.h>
+
+//C_Program_find_ascii_of_a_character:
+int main(void)
+{
+	int a,b,z;
+
+	printf("Enter value of a:\n"); /* prints Enter a value*/
+	fflush (stdout);
+	scanf (" %d",&a); // we have to leave space before scanf and not to leave space after it
+	printf("Enter value of b:\n");
+	fflush (stdout);
+	scanf (" %d",&b);
+
+	z=a;
+	a=b;
+	b=z;
+	printf("After swapping, value of a : %d\n",a);
+	printf("After swapping, value of b : %d\n",b);
+	return 0 ;
+}
